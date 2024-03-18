5f222a30624c988173a4a5c12f8bdc3c5274d98c
tejask-iprogrammer <tejask@iprogrammer.co>
2024-03-18 16:08:45 +0530

    edit image and show fetch image isse check

--- resources/views/livewire/banner/banner.blade.php
+++ resources/views/livewire/banner/banner.blade.php
@@ -479,7 +479,7 @@
                                             <!--begin::Preview existing avatar-->
                                            
                                             @if(isset($banner_name))
-                                                <div class="image-input-wrapper w-125px h-125px" style="background-image: url({{ $banner_name ? $banner_name->temporaryUrl() : asset('https://viapprewamp.viapplogs.net')}}/{{$banner_name }});"></div>
+                                                <div class="image-input-wrapper w-125px h-125px" style="background-image: url({{  $banner_name->temporaryUrl() ? $banner_name->temporaryUrl() : asset('https://viapprewamp.viapplogs.net')}}/{{$banner_name }});"></div>
 
                                             @else
                                                 <div class="image-input-wrapper w-125px h-125px" style="background-image: url({{ $banner_name ? $banner_name->temporaryUrl() : ''  }});"></div>
