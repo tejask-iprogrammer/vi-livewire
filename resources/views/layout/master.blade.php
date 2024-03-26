6f56b4b11cdbe53a0c1ed07e4306a06ba5d024b5
tejask-iprogrammer <tejask@iprogrammer.co>
2024-03-26 17:16:28 +0530

    Title changes for tab

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,7 +3,7 @@
 <!--begin::Head-->
 <head>
     <base href=""/>
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'Administrator') | iProgrammer Solutions Pvt. Ltd.</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8"/>
     <meta name="description" content=""/>
